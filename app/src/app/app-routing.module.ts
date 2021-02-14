17a959621ad749d0e0292729e02d79ce3a192094
zzoysanna <zoysanna@gmail.com>
2021-02-14 23:02:08 +0300

    create app

---
+++ app/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './components/page/page.component';
+
+const routes: Routes = [
+  {
+    path: 'page/:pageId',
+    component: PageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
